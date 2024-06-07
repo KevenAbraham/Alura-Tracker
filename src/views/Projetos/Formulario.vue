2a394f16d262bf53a21a5c9e20e82162411cd367
KevenAbraham <abraham.keven20@gmail.com>
2024-06-07 14:28:58 -0300

    feat

---
+++ src/views/Projetos/Formulario.vue
@@ -0,0 +1,71 @@
+<template>
+    <section class="projeto">
+        <h1 class="title">Projetos</h1>
+        <form @submit.prevent="salvar"> <!--Ouvir o comportamento padrão já ouvindo o comportamento de submeter e recarregar a página-->
+            <div class="field">
+                <label for="" class="label">Nome do Projeto</label>
+                <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjeto">
+            </div>
+            <div class="field">
+                <button class="button" type="submit">Salvar</button>
+            </div>
+        </form>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="projeto in projetos" :key="projeto.id">
+                    <td>{{ projeto.id }}</td>
+                    <td>{{ projeto.nome }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</template>
+
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import { useStore } from '@/store';
+
+export default defineComponent({
+    name: 'FormView',
+    props: {
+        id: {
+            type: String 
+        }
+    },
+    mounted() {
+
+    },
+    data() {
+        return {
+            nomeDoProjeto: ""
+        }
+    },
+    methods: {
+        salvar() {
+            this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+            this.nomeDoProjeto = ""
+            this.$router.push('/projetos')
+        }
+    },
+    setup() {
+        const store = useStore()
+        return {
+            store,
+            projetos: computed(() => store.state.projetos)
+        }
+    }
+})
+</script>
+
+<style scoped>
+.projeto {
+    padding: 1.25rem;
+}
+</style>
