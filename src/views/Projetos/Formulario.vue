933b7c38a9f5da487e638967a394f11b4d8f284c
Keven Abraham de Souza <abraham.keven20@gmail.com>
2024-06-13 09:26:32 -0300

    feat: ajustando classes do projeto | colocando mutation e actions separadas

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -16,10 +16,10 @@
 import { useStore } from "@/store";
 import { defineComponent } from "vue";
 
-import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes'
 import { TipoNotificacao } from "@/interfaces/INotificacao";
 
 import useNotificador from '@/hooks/notificador'
+import { CADASTRAR_PROJETO, ALTERAR_PROJETO } from "@/store/tipo-acoes";
 export default defineComponent({
   name: "FormularioView",
   props: {
@@ -29,7 +29,7 @@ export default defineComponent({
   },
   mounted() {
     if (this.id) {
-      const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
+      const projeto = this.store.state.projeto.projetos.find(proj => proj.id == this.id)
       this.nomeDoProjeto = projeto?.nome || ''
     }
   },
@@ -41,13 +41,16 @@ export default defineComponent({
   methods: {
     salvar() {
       if (this.id) {
-        this.store.commit(ALTERA_PROJETO, {
+        this.store.dispatch(ALTERAR_PROJETO, {
           id: this.id,
           nome: this.nomeDoProjeto
-        })
+        }).then(() => this.lidarComSucesso())
       } else {
-        this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
+        this.store.dispatch(CADASTRAR_PROJETO, this.nomeDoProjeto)
+          .then(() => this.lidarComSucesso())
       }
+    },
+    lidarComSucesso() {
       this.nomeDoProjeto = "";
       this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso!')
       this.$router.push('/projetos')
